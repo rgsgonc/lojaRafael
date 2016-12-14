17641dbb98fdcad07e1bee5138d41310f71bac5b
rgsgonc <rgs.gonc@gmail.com>
2016-12-14 17:53:55 -0200

    ajuste nos campos do login

--- formulario-login.php
+++ formulario-login.php
@@ -7,29 +7,29 @@
 	               	</div>
 	            </div>
 				<div class="main-login main-center">
-					<form class="form-horizontal" method="post" action="#">
+					<form class="form-horizontal" method="post" action="login.php">
 						<div class="form-group">
 							<label for="username" class="cols-sm-2 control-label">Username</label>
 							<div class="cols-sm-10">
 								<div class="input-group">
 									<span class="input-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i></span>
-									<input type="text" class="form-control" name="username" id="username"  placeholder="Enter your Username"/>
+									<input type="email" class="form-control" name="email"  placeholder="E-mail" required/>
 								</div>
 							</div>
 						</div>
 
 						<div class="form-group">
-							<label for="password" class="cols-sm-2 control-label">Password</label>
+							<label for="password" class="cols-sm-2 control-label">Senha</label>
 							<div class="cols-sm-10">
 								<div class="input-group">
 									<span class="input-group-addon"><i class="fa fa-lock fa-lg" aria-hidden="true"></i></span>
-									<input type="password" class="form-control" name="password" id="password"  placeholder="Enter your Password"/>
+									<input type="password" class="form-control" name="senha"  placeholder="Senha" required=""/>
 								</div>
 							</div>
 						</div>
 
 						<div class="form-group ">
-							<button type="button" class="btn btn-primary btn-lg btn-block login-button">Sign in</button>
+							<button class="btn btn-primary btn-lg btn-block login-button">Login</button>
 						</div>
 						<div class="login-register">
 				            <a href="create_account.php">Create account</a> or <a href="reset_password.php">reset password</a>
